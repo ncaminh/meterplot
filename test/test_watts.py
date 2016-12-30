08ffc6d0bb71a88cde84ce1d7ab2c9215578ad62
Nico Schlömer <nico.schloemer@gmail.com>
2016-12-30 23:45:45 +0100

    substantial tests

--- test/test_watts.py
+++ test/test_watts.py
@@ -1,9 +1,38 @@
+import os
+import matplotlib as mpl
+if 'DISPLAY' not in os.environ:
+    mpl.use('Agg')
+import watts
 
 
-def test_import():
-    import os
-    import matplotlib as mpl
-    if 'DISPLAY' not in os.environ:
-        mpl.use('Agg')
-    import watts
+def _show(filename):
+    unit, groups = watts.read_data(filename)
+    # add average per second between the datetimes
+    for group in groups:
+        for meter in group['meters']:
+            meter['aps'] = watts.average_per_second(
+                meter['datetimes'],
+                meter['values']
+                )
+
+    # merge across meters
+    data = []
+    for group in groups:
+        datetimes_combined, averages_combined = \
+            watts.merge_meters(group['meters'])
+        data.append({
+            'datetimes': datetimes_combined,
+            'aps': averages_combined,
+            'style': group['style'],
+            })
+
+    watts.show(unit, data)
     return
+
+
+def test_electricity():
+    _show('test_electricity.yml')
+
+
+def test_water():
+    _show('test_water.yml')
