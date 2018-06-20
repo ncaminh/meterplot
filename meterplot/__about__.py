f8b2267c77b5fd448fc6e8635ebcb2606e0d8879
Nico Schlömer <nico.schloemer@gmail.com>
2018-06-20 12:54:40 +0200

    version bump

--- meterplot/__about__.py
+++ meterplot/__about__.py
@@ -4,5 +4,5 @@ __author__ = "Nico Schlömer"
 __email__ = "nico.schloemer@gmail.com"
 __copyright__ = "Copyright (c) 2016-2018, {} <{}>".format(__author__, __email__)
 __license__ = "License :: OSI Approved :: MIT License"
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 __status__ = "Development Status :: 4 - Beta"
