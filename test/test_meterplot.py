24842b10b87039779e44e8bcd7ce079b1fcdbed5
Nico Schlömer <nico.schloemer@gmail.com>
2020-06-29 17:54:24 +0200

    fix tests

--- test/test_meterplot.py
+++ test/test_meterplot.py
@@ -1,32 +1,13 @@
-import os
+from pathlib import Path
 
 import meterplot
 
 
 def _show(filename):
-    this_dir = os.path.dirname(os.path.realpath(__file__))
-    filename = os.path.join(this_dir, filename)
-
-    unit, groups = meterplot.read_data(filename)
-    # add average per second between the datetimes
-    for group in groups:
-        for meter in group["meters"]:
-            meter["aps"] = meterplot.average_per_second(
-                meter["datetimes"], meter["values"]
-            )
-
-    # merge across meters
-    data = []
-    for group in groups:
-        datetimes_combined, averages_combined = meterplot.merge_meters(group["meters"])
-        data.append(
-            {
-                "datetimes": datetimes_combined,
-                "aps": averages_combined,
-                "style": group["style"],
-            }
-        )
-    meterplot.show(unit, data)
+    this_dir = Path(__file__).resolve().parent
+    unit, datetimes, values = meterplot.read_data(this_dir / filename)
+    averages = meterplot.average_per_second(datetimes, values)
+    meterplot.show(unit, datetimes, averages)
 
 
 def test_electricity():
